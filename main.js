a0ede29b48ba0d05d0be590c984f7024b91c3f60
DeborahOlaboye <deboraholaboye@gmail.com>
2025-02-01 20:01:35 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const questions = [
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+]

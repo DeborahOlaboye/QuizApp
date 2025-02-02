9e7810e75746f24892260928a5802ae6a555d974
DeborahOlaboye <deboraholaboye@gmail.com>
2025-02-02 22:44:08 +0100

    Updated the html file, the styling and included more questions in the main.js file

--- main.js
+++ main.js
@@ -15,4 +15,392 @@ const questions = [
             'Sergey Brin'],
         correctAnswer: 'Tim Berners-Lee'
     },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'What is HTML?',
+        answers: ['HTML describes the structure of a webpage', 
+            'HTML is the standard markup language mainly used to create web pages',
+            'HTML consists of a set of elements that helps the browser how to view the content',
+            'All of the mentioned'],
+        correctAnswer: 'All of the mentioned'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
+    {
+        question: 'Who is the father of HTML?',
+        answers: ['Rasmus Lerdorf',
+            'Tim Berners-Lee',
+            'Brendan Eich',
+            'Sergey Brin'],
+        correctAnswer: 'Tim Berners-Lee'
+    },
+    {
+        question: 'HTML stands for __________',
+        answers: ['HyperText Markup Language',
+            'HyperText Machine Language',
+            'HyperText Marking Language',
+            'HighText Marking Language'],
+        correctAnswer: 'HyperText Markup Language'
+    },
 ]
+
+
+
+
